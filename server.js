1d0ee8c29edd638e52faa596cd6c578760fe4cea
Yassine Mestaoui <yassinemestaoui98@gmail.com>
2023-05-12 14:45:20 +0100

    added env

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ app.get('', (req, res) => {
   res.send('Hi from employees app');
 });
 
-const ip = 'localhost';
-
-const port = 5000;
+const port = process.env.OPENSHIFT_NODEJS_PORT || 5000;
+const ip = process.env.OPENSHIFT_NODEJS_IP || 'localhost';
 
 app.listen(port, () => {
   console.log(`serve at http://${ip}:${port}`);
